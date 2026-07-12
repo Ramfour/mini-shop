ccded11955b477787ba4d114dbfb8293f76947ab
Ramfour <bdddanil2002@gmail.com>
2026-07-12 15:22:22 +0700

    add data in shop, new function formatMoney

--- analytics.php
+++ analytics.php
@@ -1,2 +1,9 @@
 <?php 
 // Вспомогательные функции для аналитики
+
+// Форматировать копейки в рубли
+function formatMoney(int $kopecks): string {
+    $rubles = floor($kopecks / 100);
+    $remainingKopecks = $kopecks % 100;
+    return sprintf("%d руб. %02d коп.", $rubles, $remainingKopecks);
+}
--- shop.php
+++ shop.php
@@ -2,6 +2,46 @@
 // данные
 require 'analytics.php';
 
+// Массив продуктов
+$products = [
+    [
+        'name'     => 'Кофе',
+        'price'    => 19999,
+        'count'    => 2,
+        'category' => 'drink',
+    ],
+    [
+        'name'     => 'Чай',
+        'price'    => 9999,
+        'count'    => 1,
+        'category' => 'drink',
+    ],
+    [
+        'name'     => 'Печенье',
+        'price'    => 4999,
+        'count'    => 3,
+        'category' => 'food',
+    ],
+    [
+        'name'     => 'Сок',
+        'price'    => 2999,
+        'count'    => 5,
+        'category' => 'drink',
+    ],
+    [
+        'name'     => 'Торт',
+        'price'    => 59999,
+        'count'    => 1,
+        'category' => 'food',
+    ],
+];
+
+// Массив категорий
+$categories = [
+    'drink' => 'Напитки',
+    'food'  => 'Еда',
+    'other' => 'Другое',
+];
 
 
 

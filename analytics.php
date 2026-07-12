b2c3f634af2ed8b0d51889a6f1d04db8b4ffcce6
Ramfour <bdddanil2002@gmail.com>
2026-07-12 19:01:58 +0700

    add calculateTotals

--- analytics.php
+++ analytics.php
@@ -3,7 +3,33 @@
 
 // Форматировать копейки в рубли
 function formatMoney(int $kopecks): string {
-    $rubles = floor($kopecks / 100);
+    $rubles = intdiv($kopecks, 100);
     $remainingKopecks = $kopecks % 100;
     return sprintf("%d руб. %02d коп.", $rubles, $remainingKopecks);
 }
+
+// Рассчет суммы, наличия скидки, НДС и количества товаров
+function calculateTotals(array $products): array {
+    $itemSum = 0;
+    $itemNds = 0;
+    $itemCount = 0;
+    $hasDiscount = false;
+    foreach ($products as $product) {
+        $itemSum += $product['price'] * $product['count'];
+        $itemNds += ($product['price'] * $product['count']) * 0.2; // 20% НДС
+        $itemCount += $product['count'];
+    }
+    $hasDiscount = $itemSum > 300000;
+    if ($hasDiscount) {
+    $discountAmount = intval($itemSum * 0.1);
+    $itemSum = $itemSum - $discountAmount;
+    }
+    return [
+        'discount' => $hasDiscount,
+        'sum' => $itemSum,
+        'nds' => $itemNds,
+        'count' => $itemCount
+    ];
+}
+
+
--- receipt.php
+++ receipt.php
@@ -1,2 +1,5 @@
 <?php 
 // Вывод чека
+$title      = 'чек заказа';
+$titleUpper = mb_strtoupper($title, 'UTF-8');   // mb_strtoupper
+$titleLength = mb_strlen($titleUpper, 'UTF-8'); // mb_strlen
